0ea3abe65322a6c8d6b0de5d64a03aafc9c6ed96
pcbean <pcbean@gmail.com>
2024-01-06 12:37:35 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd6eef12b-574c-4efb-8fcc-06e036db9e1b';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
